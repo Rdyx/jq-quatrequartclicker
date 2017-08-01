ab6e6145854d3d02a9da916d941ce30b203fcc9f
Rdyx <tiranico.pro@gmail.com>
2017-08-01 12:25:37 +0200

    finish

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-$(document).ready(function(){
-	//votre code
-
-
+$(document).ready(function () {
+	var theArray = ["Leo#404", "Ah t'es le cadet... T'es majeur ?", "Mais t'es sûr que t'as push ?"];
+	var compteur = 0;
+	$(".cake").click(function () {
+		var randomize = theArray[Math.floor(theArray.length * Math.random())];
+		$(".counter").html(++compteur);
+		if (compteur % 3 === 0) {
+			alert(randomize);
+		};
+	});
 });
